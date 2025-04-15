a8b76358ada6b6faef4b76d16da98953e160c1e3
Eugene Wekesa <eugenco578@gmail.com>
2025-04-15 23:16:02 +0300

    lists and keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   return (
-    <div className="project-item">
+    <div>
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div>
+        {technologies.map((tech, index) => (
+          <span key={index}>{tech}</span>
+        ))}
       </div>
     </div>
   );
